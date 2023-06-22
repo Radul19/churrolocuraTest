4d4a86b6489ed9c487b7c5717d25fbc2219a527a
Radul19 <radulito19@gmail.com>
2023-06-22 11:33:53 -0400

    clean

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,69 +1,11 @@
-<!-- <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
-</script>
-
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style> -->
 <script>
     export const prerender = true;
     import { onMount } from "svelte";
     import loading from "../public/loading.gif";
   
-    let iw=0;
-    let ih=1;
+      import { browser } from "$app/environment";
+    let iw;
+    let ih;
     onMount(() => {
       let desktop = iw > ih && true;
   
@@ -75,11 +17,11 @@
     });
   </script>
   
-  <svelte:window bind:innerWidth={iw} bind:innerHeight={ih} />
+  <!-- <svelte:window bind:innerWidth={iw} bind:innerHeight={ih} /> -->
   <div class="">
-      <!-- <p>Visit <a href="/mobile/home">Mobile folder</a> to read the documentation</p> -->
+      <p>Visit <a href="/mobile/home">Mobile folder</a> to read the documentation</p>
     <!-- <p>Loading...</p> -->
-    <img src={loading} alt="laoding" />
+    <!-- <img src={loading} alt="laoding" /> -->
   </div>
   
   
